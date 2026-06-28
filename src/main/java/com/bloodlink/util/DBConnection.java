3107660e946f9c6c5f7acf481f12242adf3e8bc9
Rani Aghav <raniaghav07@gmail.com>
2026-06-28 12:25:36 +0530

    Fix: Finalized API relative paths in bloodlink.war

--- src/main/java/com/bloodlink/util/DBConnection.java
+++ src/main/java/com/bloodlink/util/DBConnection.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
  */
 public class DBConnection {
 
-       private static final String DB_URL = "jdbc:mysql://://aivencloud.com";
+    private static final String DB_URL = "jdbc:mysql://bloodlink-db-raniaghav07-6151.l.aivencloud.com:24588/defaultdb?sslMode=REQUIRED";
     private static final String DB_USER = "avnadmin";
     private static final String DB_PASS = "AVNS_Bj76WQ8KMI1buo57mb6";
 
